b49d7b65f66206f0da6c1dd2c73bf4b3eeb07d30
R Santhosh <r.santhosh@nagarro.com>
2024-09-04 11:39:30 +0530

    Remove api key from git
    Signed-off-by: R Santhosh <r.santhosh@nagarro.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 
 export const environment = {
-    googleMapsApiKey: 'AIzaSyCGojJN7DE7tM2yEa-oTX2lmVICz8d4-RM',
-    solarApiKey:  'AIzaSyCGojJN7DE7tM2yEa-oTX2lmVICz8d4-RM',
+    googleMapsApiKey: '',
+    solarApiKey:  '',
     solarApi: 'https://solar.googleapis.com/v1/buildingInsights:findClosest?location.latitude=${latitude}&location.longitude=${longitude}&requiredQuality=HIGH'
 };
